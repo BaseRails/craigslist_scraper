ab1046f413be369e0a1b2ef9a37e1ef5a9683795
Alex Yang <alex@baserails.com>
2014-11-16 17:57:39 -0500

    Add old migration

--- db/migrate/20141114214737_create_images.rb
+++ db/migrate/20141114214737_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-      t.text :url
+      t.string :url
       t.references :post, index: true
 
       t.timestamps
